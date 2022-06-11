740f156267949b29f9bf85e3ab10219372348f0f
Tahir Mbaye <mbayepro6@gmail.com>
2022-06-11 01:29:47 +0000

    MM

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class ClasseController extends AbstractController
 {
